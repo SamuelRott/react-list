e8a3e72e69c4855f55ae15838743af4778035629
Samuel <rott.samuel@gmail.com>
2017-05-23 11:04:42 +0200

    add currentFriends logic

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -14,9 +14,18 @@ class FriendList extends Component {
 
   render () {
 
+    // maximum number of friends per page
     const friendsPerPage = 2;
     const friends = this.props.friends;
     const currentPage = this.state.currentPage;
+
+    // last friend show on a page, -1 with slice()
+    const lastFriend = currentPage * friendsPerPage;
+    // first friend show on a page
+    const firstFriend = lastFriend - friendsPerPage;
+    // friends that will be show per page
+    const currentFriends = friends.slice(firstFriend, lastFriend);
+    console.log(currentFriends);
 
     const displayFriends = friends.map((friend, index) => {
       return (
