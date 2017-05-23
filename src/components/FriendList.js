6fad21007042ef911f3e653befefe6aef0fcd77b
Samuel <rott.samuel@gmail.com>
2017-05-23 10:53:37 +0200

    refactor display FriendListItem + setup state and const for pagination

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -3,21 +3,35 @@ import styles from './FriendList.css';
 import FriendListItem from './FriendListItem';
 
 class FriendList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1
+    };
+  }
+
+
   render () {
+
+    const friendsPerPage = 2;
+    const friends = this.props.friends;
+    const currentPage = this.state.currentPage;
+
+    const displayFriends = friends.map((friend, index) => {
+      return (
+        <FriendListItem
+          key={index}
+          id={index}
+          name={friend.name}
+          starred={friend.starred}
+          {...this.props.actions} />
+      );
+    })
+
     return (
       <ul className={styles.friendList}>
-        {
-          this.props.friends.map((friend, index) => {
-            return (
-              <FriendListItem
-                key={index}
-                id={index}
-                name={friend.name}
-                starred={friend.starred}
-                {...this.props.actions} />
-            );
-          })
-        }
+        { displayFriends }
       </ul>
     );
   }
