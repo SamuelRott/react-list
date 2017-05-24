a5b484cf2dc48451634c2f7fb07e934462411725
Samuel <rott.samuel@gmail.com>
2017-05-24 15:24:35 +0200

    start tests using jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+
+
+import App from './containers/App';
+import FriendListApp from './containers/FriendListApp';
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it( 'App component should contain FriendListApp component', () =>
+{
+    const wrapper = shallow( <App /> );
+    expect( wrapper.contains( <FriendListApp /> ) );
 });
